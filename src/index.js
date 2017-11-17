a99f59963a4ba05102a18310336c0b9271c7c94d
Jonas Enge <jonas.enge@t-fk.no>
2017-11-17 23:42:30 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -18,47 +18,49 @@ const DEFAULT_ALLOW_HEADERS = [
 
 const DEFAULT_MAX_AGE_SECONDS = 60 * 60 * 24 // 24 hours
 
-module.exports = exports = options => handler => (req, res, ...restArgs) => {
-  const {
-    maxAge,
-    origin,
-    allowHeaders,
-    exposeHeaders,
-    allowMethods
-  } = (options || {})
+module.exports = exports = options => fn => {
+  return (req, res) => {
+    const {
+      maxAge,
+      origin,
+      allowHeaders,
+      exposeHeaders,
+      allowMethods
+    } = (options || {})
 
-  res.setHeader(
-    'Access-Control-Max-Age',
-    '' + (maxAge || DEFAULT_MAX_AGE_SECONDS)
-  )
-
-  res.setHeader(
-    'Access-Control-Allow-Origin',
-    (origin || '*')
-  )
-
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    (allowMethods || DEFAULT_ALLOW_METHODS).join(',')
-  )
-
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    (allowHeaders || DEFAULT_ALLOW_HEADERS).join(',')
-  )
-
-  if (exposeHeaders && exposeHeaders.length) {
     res.setHeader(
-      'Access-Control-Expose-Headers',
-      exposeHeaders.join(',')
+      'Access-Control-Max-Age',
+      '' + (maxAge || DEFAULT_MAX_AGE_SECONDS)
     )
+
+    res.setHeader(
+      'Access-Control-Allow-Origin',
+      (origin || '*')
+    )
+
+    res.setHeader(
+      'Access-Control-Allow-Methods',
+      (allowMethods || DEFAULT_ALLOW_METHODS).join(',')
+    )
+
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      (allowHeaders || DEFAULT_ALLOW_HEADERS).join(',')
+    )
+
+    if (exposeHeaders && exposeHeaders.length) {
+      res.setHeader(
+        'Access-Control-Expose-Headers',
+        exposeHeaders.join(',')
+      )
+    }
+
+    res.setHeader('Access-Control-Allow-Credentials', 'true')
+
+    if (req.method === 'OPTIONS') {
+      return {}
+    }
+
+    return fn(req, res)
   }
-
-  res.setHeader('Access-Control-Allow-Credentials', 'true')
-
-  if (req.method === 'OPTIONS') {
-    return {}
-  }
-
-  return handler(req, res, ...restArgs)
 }
